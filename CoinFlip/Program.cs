6ef7992c61d75759e2fa9eab72e689ec24dcf152
Yuriy Yarytskyy <iamfromukraine@gmail.com>
2021-08-04 23:18:21 -0400

    initial commit

---
+++ CoinFlip/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoinFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random coinFlip = new Random();
+            int flip = coinFlip.Next(0, 2);
+            if (flip == 1){
+                Console.WriteLine("Tails");
+            }
+            else {
+                Console.WriteLine("Heads");
+                }
+        }
+    }
+}
+
+//OR
+
+// Random coin = new Random();
+// int flip = coin.Next(0, 2);
+// Console.WriteLine((flip == 0) ? "heads" : "tails");
+
+//OR
+
+// Random coin = new Random();
+// Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
