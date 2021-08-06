e5d9f3cda0bceec6c20b40c40f4c41ed070cc7f3
Yuriy Yarytskyy <iamfromukraine@gmail.com>
2021-08-06 09:49:58 -0400

    adding some files

---
+++ CompareTriplets/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace CompareTriplets
+{
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        int alice = 0;
+        int bob = 0;
+        List<int> score = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            if (a.get(i) > b.get(i)) alice++;
+            if (a.get(i) < b.get(i)) bob++;
+        } 
+        score.add(0,alice);
+        score.add(1,bob);
+        return score;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = Result.compareTriplets(a, b);
+
+            textWriter.WriteLine(String.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
+
