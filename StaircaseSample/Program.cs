b3aaca84417284a01fc133b90b1cf726e03ba0cb
Yuriy Yarytskyy <iamfromukraine@gmail.com>
2021-08-10 21:18:02 -0400

    adding staircase excersise

---
+++ StaircaseSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StaircaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Random random = new Random();
+            int n = 20; 
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = n - 1; i >= 0; i--)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                
+                Console.WriteLine("");
+            }
+
+        }
+        
+    }
+}
